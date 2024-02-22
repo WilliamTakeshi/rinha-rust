eb70826d949e9dfeab92d23bf2d28f2f6abba28b
William Takeshi <williamtpereira@gmail.com>
2024-02-21 22:28:29 -0300

    add sql and tables

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,75 @@
+use std::str::FromStr;
+
 use axum::{routing::get, Router};
-use serde::Serialize;
+use serde::{Deserialize, Serialize};
+use time::PrimitiveDateTime;
 use tracing::subscriber::set_global_default;
 use tracing_bunyan_formatter::{BunyanFormattingLayer, JsonStorageLayer};
 use tracing_log::LogTracer;
 use tracing_subscriber::{layer::SubscriberExt, EnvFilter, Registry};
 
-#[derive(Serialize)]
+#[derive(Deserialize, Serialize)]
+struct PostTransaction {
+    value: u64,
+    kind: TransactionKind,
+    description: String,
+}
+
+#[derive(Deserialize, Serialize)]
 struct Transaction {
-    id: u64,
-    username: String,
+    #[serde(rename = "valor")]
+    value: u64,
+    #[serde(rename = "tipo")]
+    kind: TransactionKind,
+    #[serde(rename = "descricao")]
+    description: String,
+    #[serde(rename = "realizada_em")]
+    inserted_at: PrimitiveDateTime,
+}
+
+enum TransactionKind {
+    Credit,
+    Debit,
+}
+
+impl Serialize for TransactionKind {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: serde::Serializer,
+    {
+        serializer.serialize_str(&self.to_string())
+    }
+}
+
+impl<'de> Deserialize<'de> for TransactionKind {
+    fn deserialize<D>(deserializer: D) -> Result<TransactionKind, D::Error>
+    where
+        D: serde::Deserializer<'de>,
+    {
+        let s = String::deserialize(deserializer)?;
+        TransactionKind::from_str(&s).map_err(serde::de::Error::custom)
+    }
+}
+
+impl FromStr for TransactionKind {
+    type Err = String;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s {
+            "c" => Ok(TransactionKind::Credit),
+            "d" => Ok(TransactionKind::Debit),
+            _ => Err(format!("Invalid transaction kind: {}", s)),
+        }
+    }
+}
+
+impl ToString for TransactionKind {
+    fn to_string(&self) -> String {
+        match self {
+            TransactionKind::Credit => "c".to_string(),
+            TransactionKind::Debit => "d".to_string(),
+        }
+    }
 }
 
 #[tokio::main]
